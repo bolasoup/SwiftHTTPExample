2ce20d4c9705061dd9674404eefe3c55de299b1e
Eric Larson <eclarson@smu.edu>
2024-06-04 23:12:55 -0500

    Updating example to use fastapi as ReST server.

---
+++ HTTPSwiftExample/ClientModel.swift
@@ -0,0 +1,13 @@
+//
+//  ClientModel.swift
+//  HTTPSwiftExample
+//
+//  Created by Eric Cooper Larson on 6/3/24.
+//  Copyright © 2024 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class ClientModel: NSObject {
+
+}
