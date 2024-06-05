8ab6a0a7bbd0209e06fe425e6429250fbf768f4a
Eric Larson <eclarson@smu.edu>
2024-06-04 23:15:03 -0500

    Changed all methods to work with fastapi

--- HTTPSwiftExample/ClientModel.swift
+++ HTTPSwiftExample/ClientModel.swift
@@ -6,8 +6,236 @@
 //  Copyright © 2024 Eric Larson. All rights reserved.
 //
 
+protocol ClientDelegate{
+    func receiveResponse(response:Any, strData:Any)
+}
+
+enum RequestEnum:String {
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
+
 import UIKit
 
-class ClientModel: NSObject {
+class ClientModel: NSObject, URLSessionDelegate {
+    
+    //MARK: Properties and Delegation
+    let operationQueue = OperationQueue()
+    var server_ip = "127.0.0.1" // this will be the default ip
+    // create a delegate for using the protocol
+    var delegate:ClientDelegate?
+    
+    lazy var session = {
+        let sessionConfig = URLSessionConfiguration.ephemeral
+        
+        sessionConfig.timeoutIntervalForRequest = 5.0
+        sessionConfig.timeoutIntervalForResource = 8.0
+        sessionConfig.httpMaximumConnectionsPerHost = 1
+        
+        let tmp = URLSession(configuration: sessionConfig,
+            delegate: self,
+            delegateQueue:self.operationQueue)
+        
+        return tmp
+        
+    }()
+    
+    //MARK: Setters and Getters
+    func setServerIp(ip:String)->(Bool){
+        // user is trying to set ip: make sure that it is valid ip address
+        if matchIp(for:"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.|$)){4}", in: ip){
+            server_ip = ip
+            // return success
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    
+    //MARK: Main Functions
+    
+    // Call Hello World on the App
+    func connectToRoot(){
+        let baseURL = "http://\(server_ip):8000/"
+        let postUrl = URL(string: "\(baseURL)")
+        
+        // create a custom HTTP POST request
+        var request = URLRequest(url: postUrl!)
+        
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let postTask : URLSessionDataTask = self.session.dataTask(with: request,
+                        completionHandler:{(data, response, error) in
+            // TODO: handle error!
+            let jsonDictionary = self.convertDataToDictionary(with: data)
+                            
+            if let delegate = self.delegate, let resp=response {
+                delegate.receiveResponse(response: resp, strData: jsonDictionary)
+            }
 
+        })
+        
+        postTask.resume() // start the task
+        
+    }
+    
+    
+    // List all the Characters in the app
+    func listCharacters(){
+        let baseURL = "http://\(server_ip):8000/characters"
+        let postUrl = URL(string: "\(baseURL)")
+        
+        // create a custom HTTP POST request
+        var request = URLRequest(url: postUrl!)
+        
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let postTask : URLSessionDataTask = self.session.dataTask(with: request,
+                        completionHandler:{(data, response, error) in
+            // TODO: handle error!
+            let jsonDictionary = self.convertDataToDictionary(with: data)
+                            
+            if let delegate = self.delegate, let resp=response {
+                delegate.receiveResponse(response: resp, strData: jsonDictionary)
+            }
+
+        })
+        
+        postTask.resume() // start the task
+        
+    }
+    
+    
+    func addCharacter(_ character:[String: Any]){
+        let baseURL = "http://\(server_ip):8000/characters"
+        let postUrl = URL(string: "\(baseURL)")
+        
+        // create a custom HTTP POST request
+        var request = URLRequest(url: postUrl!)
+        
+        // utility method to use from below
+        let requestBody:Data = try! JSONSerialization.data(withJSONObject: character)
+        
+        // The Type of the request is given here
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = requestBody
+        
+        let postTask : URLSessionDataTask = self.session.dataTask(with: request,
+                        completionHandler:{(data, response, error) in
+            // TODO: handle error!
+            let jsonDictionary = self.convertDataToDictionary(with: data)
+                            
+            if let delegate = self.delegate {
+                delegate.receiveResponse(response: response!, strData: jsonDictionary)
+            }
+
+        })
+        
+        postTask.resume() // start the task
+    }
+    
+    func updateCharacter(_ character:[String: Any]){
+        self.restUtility(character, requestType: .put)
+    }
+    
+    func findCharacter(_ character:[String: Any]){
+        self.restUtility(character, requestType: .get)
+    }
+    
+    func deleteCharacter(_ character:[String: Any]){
+        self.restUtility(character, requestType: .delete)
+    }
+    
+    
+    private func restUtility(_ character:[String: Any], requestType:RequestEnum){
+        guard let name = character["name"] else{
+            return
+        }
+        let baseURL = "http://\(server_ip):8000/characters/\(name)"
+        let postUrl = URL(string: "\(baseURL)")
+        
+        // create a custom HTTP POST request
+        var request = URLRequest(url: postUrl!)
+        
+        // utility method to use from below
+        let requestBody:Data = try! JSONSerialization.data(withJSONObject: character)
+        
+        // The Type of the request is given here
+        request.httpMethod = requestType.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        if requestType != .get{
+            // if not a get request, then we need a body
+            // otherwise we cannot add a body to a get request
+            request.httpBody = requestBody
+        }
+        
+        let postTask : URLSessionDataTask = self.session.dataTask(with: request,
+                        completionHandler:{(data, response, error) in
+            // TODO: handle error!
+            // any URL or domain errors will not be caught
+            
+            var jsonDictionary = [String:Any]()
+            if requestType != .delete{
+                jsonDictionary = self.convertDataToDictionary(with: data)
+            }
+                            
+            if let delegate = self.delegate {
+                delegate.receiveResponse(response: response!, strData: jsonDictionary)
+            }
+
+        })
+        
+        postTask.resume() // start the task
+    }
+
+    
+    //MARK: Utility Functions
+    private func matchIp(for regex:String, in text:String)->(Bool){
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let results = regex.matches(in: text, range: NSRange(text.startIndex..., in: text))
+            if results.count > 0{return true}
+            
+        } catch _{
+            return false
+        }
+        return false
+    }
+    
+    private func convertDictionaryToData(with jsonUpload:NSDictionary) -> Data?{
+        // convenience function for serialiing an NSDictionary
+        do { // try to make JSON and deal with errors using do/catch block
+            let requestBody = try JSONSerialization.data(withJSONObject: jsonUpload, options:JSONSerialization.WritingOptions.prettyPrinted)
+            return requestBody
+        } catch {
+            print("json error: \(error.localizedDescription)")
+            return nil
+        }
+    }
+    
+
+    
+    private func convertDataToDictionary(with data:Data?)->[String:Any]{
+        // convenience function for getting Dictionary from server data
+        do { // try to parse JSON and deal with errors using do/catch block
+            let jsonDictionary: [String:Any] =
+                try JSONSerialization.jsonObject(with: data!,
+                                                 options: JSONSerialization.ReadingOptions.mutableContainers) as! [String : Any]
+            
+            return jsonDictionary
+            
+        } catch {
+            print("json error: \(error.localizedDescription)")
+            if let strData = String(data:data!, encoding:String.Encoding(rawValue: String.Encoding.utf8.rawValue)){
+                print("printing JSON received as string: "+strData)
+            }
+            return [String:Any]() // just return empty
+        }
+    }
 }
--- HTTPSwiftExample/ViewController.swift
+++ HTTPSwiftExample/ViewController.swift
@@ -14,15 +14,18 @@
 
 import UIKit
 
-class ViewController: UIViewController, URLSessionDelegate {
+class ViewController: UIViewController, ClientDelegate {
     
     //MARK: Properties
-    var floatValue = 5.5
-    let operationQueue = OperationQueue()
+    let client = ClientModel() // how we will interact with the server
     
     //MARK: View Outlets
     @IBOutlet weak var mainTextView: UITextView!
     @IBOutlet weak var ipAddressTextView: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var powerTextField: UITextField!
+    @IBOutlet weak var kindTextField: UITextField!
+    @IBOutlet weak var levelSlider: UISlider!
     
     //MARK: Lazy Computed Properties
     lazy var animation = {
@@ -33,138 +36,73 @@ class ViewController: UIViewController, URLSessionDelegate {
         tmp.duration = 0.5
         return tmp
     }()
-    
-    lazy var SERVER_URL = {
-        //setup default server IP (if not entered by user)
-        let default_ip = "10.9.191.61"
-        let tmp = "http://\(default_ip):8000"
-        
-        DispatchQueue.main.async {
-            self.ipAddressTextView.text = default_ip
-        }
-        
-        return tmp
-    }()
-    
-    lazy var session = {
-        let sessionConfig = URLSessionConfiguration.ephemeral
-        
-        sessionConfig.timeoutIntervalForRequest = 5.0
-        sessionConfig.timeoutIntervalForResource = 8.0
-        sessionConfig.httpMaximumConnectionsPerHost = 1
-        
-        let tmp = URLSession(configuration: sessionConfig,
-            delegate: self,
-            delegateQueue:self.operationQueue)
-        
-        return tmp
-        
-    }()
-    
+      
     
     //MARK: View Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // be the delegate for this text field
+        // be the delegate various things
         self.ipAddressTextView.delegate = self
+        self.nameTextField.delegate = self
+        self.powerTextField.delegate = self
+        self.kindTextField.delegate = self
+        self.client.delegate = self
         
-        
+        // setup the class for session
+        self.ipAddressTextView.text = client.server_ip
     }
     
+    //MARK: View Actions
+    @IBAction func helloWorld(_ sender: AnyObject) {
+        client.connectToRoot()
+    }
+    
+    @IBAction func listCharacters(_ sender: AnyObject) {
+        client.listCharacters()
+    }
+    
+    
+    @IBAction func createRequest(_ sender: AnyObject) {
+        if let character = getCharacterAttributes(){
+            client.addCharacter(character)
+        }
+    }
+    
+    @IBAction func getRequest(_ sender: AnyObject) {
+        if let character = getCharacterAttributes(){
+            client.findCharacter(character)
+        }
+    }
+    
+    @IBAction func updateRequest(_ sender: AnyObject) {
+        if let character = getCharacterAttributes(){
+            client.updateCharacter(character)
+        }
+    }
+    
+    @IBAction func deleteRequest(_ sender: AnyObject) {
+        if let character = getCharacterAttributes(){
+            client.deleteCharacter(character)
+        }
+    }
 
-    //MARK: Get Request
-    @IBAction func sendGetRequest(_ sender: AnyObject) {
-        // create a GET request and get the reponse back as NSData
-        let baseURL = "\(SERVER_URL)/GetExample"
-        let query = "?arg=\(self.floatValue)"
+    //MARK: Utility
+    func getCharacterAttributes() -> [String:Any]?{
         
-        let getUrl = URL(string: "\(baseURL)\(query)")
-        let request: URLRequest = URLRequest(url: getUrl!)
-        let dataTask : URLSessionDataTask = self.session.dataTask(with: request,
-            completionHandler:{(data, response, error) in
+        let level = Int(levelSlider.value)
+        if let name = nameTextField.text,
+           let power = powerTextField.text,
+           let kind = kindTextField.text{
             
-                // TODO: handle error!
-                print("Response:\n\(response!)")
-                let strData = String(data:data!, encoding:String.Encoding(rawValue: String.Encoding.utf8.rawValue))
-                
-                self.displayMainTextView(response: response!,
-                                         strData: strData!)
-        })
-        
-        dataTask.resume() // start the task
-        
+            // if here, all the attributes worked out
+            let tmpDict:[String:Any] = ["name":name, "power":power, "kind":kind, "level": level]
+            return tmpDict
+            
+        }
+        return nil
     }
-    
-    //MARK: Post Request, args as string in data
-    @IBAction func sendPostRequest(_ sender: AnyObject) {
-        
-        let baseURL = "\(SERVER_URL)/DoPost"
-        let postUrl = URL(string: "\(baseURL)")
-        
-        // create a custom HTTP POST request
-        var request = URLRequest(url: postUrl!)
-        
-        // data to send in body of post request (style of get arguments)
-        let requestBody:Data? = "arg1=\(self.floatValue)".data(using: String.Encoding.utf8, allowLossyConversion: false)
-        
-        request.httpMethod = "POST"
-        request.httpBody = requestBody
-        
-        let postTask : URLSessionDataTask = self.session.dataTask(with: request,
-            completionHandler:{(data, response, error) in
-                // TODO: handle error!
-                print("Response:\n\(response!)")
-                let jsonDictionary = self.convertDataToDictionary(with: data)
-                print("\n\nJSON Data:\n%@",jsonDictionary)
-                
-                self.displayMainTextView(response: response!,
-                                         strData: jsonDictionary)
-
-        })
-        
-        postTask.resume() // start the task
-    }
-    
-    //MARK: Post Request, args in request body (preferred)
-    @IBAction func sendPostWithJsonInBody(_ sender: AnyObject) {
-        
-        let baseURL = "\(SERVER_URL)/PostWithJson"
-        let postUrl = URL(string: "\(baseURL)")
-        
-        // create a custom HTTP POST request
-        var request = URLRequest(url: postUrl!)
-        
-        // data to send in body of post request (send arguments as json)
-        let jsonUpload:NSDictionary = [
-            "arg": [3.2,self.floatValue*2,self.floatValue],
-            "arg2":["CoronaVirus","NO",2021] as [Any],
-            "arg3":["EricLarson","YES",2022] as [Any],
-            "arg4":["XDD","LM*O",2023] as [Any]
-        ]
-        
-        // utility method to use from below
-        let requestBody:Data? = self.convertDictionaryToData(with:jsonUpload)
-    
-        request.httpMethod = "POST"
-        request.httpBody = requestBody
-        
-        let postTask : URLSessionDataTask = self.session.dataTask(with: request,
-                        completionHandler:{(data, response, error) in
-            print("Response:\n%@",response!)
-            let jsonDictionary = self.convertDataToDictionary(with: data)
-            print("\n\nJSON Data:\n%@",jsonDictionary)
-                            
-            self.displayMainTextView(response: response!,
-                                     strData: jsonDictionary)
-
-        })
-        
-        postTask.resume() // start the task
-        
-    }
-
 }
 
 
@@ -175,38 +113,46 @@ class ViewController: UIViewController, URLSessionDelegate {
 extension ViewController: UITextFieldDelegate{
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if let ip = textField.text{
-            // make sure ip is formatted correctly
-            if matchIp(for:"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.|$)){4}", in: ip){
-                SERVER_URL = "http://\(ip):8000"
-                print(SERVER_URL)
-            }else{
-                print("invalid ip entered")
+        
+        //TODO: setup responders in order
+        // name->power->kind-resign
+        // ip->resign
+        switch textField {
+        case nameTextField:
+            // this is the name, go to power
+            powerTextField.becomeFirstResponder()
+        case powerTextField:
+            kindTextField.becomeFirstResponder()
+        case kindTextField:
+            textField.resignFirstResponder()
+        case ipAddressTextView:
+            // update the ip address
+            if let ipString = textField.text{
+                // change the ip of server
+                if client.setServerIp(ip: ipString){
+                    // if successful, show the ip
+                    DispatchQueue.main.async {
+                        self.ipAddressTextView.text = ipString
+                    }
+                }
             }
-            
+            textField.resignFirstResponder()
+        default:
+            textField.resignFirstResponder()
         }
-        textField.resignFirstResponder()
+        
+        
         return true
     }
     
-    func matchIp(for regex:String, in text:String)->(Bool){
-        do {
-            let regex = try NSRegularExpression(pattern: regex)
-            let results = regex.matches(in: text, range: NSRange(text.startIndex..., in: text))
-            if results.count > 0{return true}
-            
-        } catch _{
-            return false
-        }
-        return false
-    }
+    
 }
 
 
 //MARK: Utility Extending Functions
 extension ViewController {
-    
-    func displayMainTextView(response:Any, strData:Any){
+        
+    func receiveResponse(response:Any, strData:Any){
         // convenience function meant for displaying the response and the
         // extra argument data from an HTTP request completion
         DispatchQueue.main.async{
@@ -215,34 +161,7 @@ extension ViewController {
         }
     }
     
-    func convertDictionaryToData(with jsonUpload:NSDictionary) -> Data?{
-        // convenience function for serialiing an NSDictionary
-        do { // try to make JSON and deal with errors using do/catch block
-            let requestBody = try JSONSerialization.data(withJSONObject: jsonUpload, options:JSONSerialization.WritingOptions.prettyPrinted)
-            return requestBody
-        } catch {
-            print("json error: \(error.localizedDescription)")
-            return nil
-        }
-    }
     
-    func convertDataToDictionary(with data:Data?)->NSDictionary{
-        // convenience function for getting Dictionary from server data
-        do { // try to parse JSON and deal with errors using do/catch block
-            let jsonDictionary: NSDictionary =
-                try JSONSerialization.jsonObject(with: data!,
-                                              options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
-            
-            return jsonDictionary
-            
-        } catch {
-            print("json error: \(error.localizedDescription)")
-            if let strData = String(data:data!, encoding:String.Encoding(rawValue: String.Encoding.utf8.rawValue)){
-                print("printing JSON received as string: "+strData)
-            }
-            return NSDictionary() // just return empty
-        }
-    }
 }
 
 
